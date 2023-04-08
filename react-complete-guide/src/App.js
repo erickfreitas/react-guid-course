c110d924929629b423334be78a82d01decb61a50
erickfreitas <erickfreitasbezerra@gmail.com>
2023-04-08 19:22:33 -0300

    criando componente de filtro

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import ExpensesFilter from "./components/Expenses/ExpensesFilter";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2022');
   const expenses = props.expenses;
+
+  const changeExpensesFilterHandler = (year) => {
+    console.log(year);
+    setFilteredYear(year);
+  }
 
   return (
     <Card className="expenses">
+      <ExpensesFilter 
+        selectedYear={filteredYear}
+        onChangeExpensesFilter={changeExpensesFilterHandler}/>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
