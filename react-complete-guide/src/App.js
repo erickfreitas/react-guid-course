598c7840f85aa6d0c038f0fd1dc28a8765375e2f
erickfreitas <erickfreitasbezerra@gmail.com>
2023-04-08 17:07:41 -0300

    passando informação de componente filho para o pai

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -24,9 +24,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expenses);
+  }
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenses={expenses}/>
     </div>
   );
--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,20 @@
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enterdExpenseData) => {
+        const expenseData = {
+            ...enterdExpenseData,
+            id: Math.random().toString()
+        }
+
+        console.log(expenseData);
+        props.onAddExpense(expenseData);
+    }
+
     return <div className='new-expense'>
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </div>
 }
 
