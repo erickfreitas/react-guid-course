fa66994345742a3f00e0b017642ee17c6a6216f1
erickfreitas <erickfreitasbezerra@gmail.com>
2023-05-31 05:47:52 -0300

    criando componente Button

--- users-register-app/src/components/Users/AddUser.js
+++ users-register-app/src/components/Users/AddUser.js
@@ -1,21 +1,35 @@
+import { useState } from 'react';
+import Button from '../UI/Button';
 import Card from '../UI/Card';
 
 import styles from './AddUser.module.css';
 
 const AddUser = props => {
 
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
         <Card className={styles.input} >
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={usernameChangeHandler} />
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
-                <button type="submit">Add User</button>
+                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     )
