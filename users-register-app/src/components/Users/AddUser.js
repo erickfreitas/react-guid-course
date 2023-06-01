c5dacc3c84d79b09f68b71dc60fb467524d726e0
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-01 06:32:59 -0300

    componente Wrapper

--- users-register-app/src/components/Users/AddUser.js
+++ users-register-app/src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal'
 
 import styles from './AddUser.module.css';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = props => {
 
@@ -52,7 +53,7 @@ const AddUser = props => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={styles.input} >
         <form onSubmit={addUserHandler}>
@@ -73,7 +74,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   )
 }
 
