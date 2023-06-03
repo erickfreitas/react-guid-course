40f857d334779da81803a3c48469dc5532b6b6d5
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-03 18:15:40 -0300

    utilizando context api

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './contexts/auth-context';
 
 function App() {
 
@@ -30,13 +31,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
