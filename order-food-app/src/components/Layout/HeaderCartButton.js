fb3de67e9126cd1221d45db865266992b225229d
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-09 16:01:36 -0300

    animando botão de carrinho através de useEffect

--- order-food-app/src/components/Layout/HeaderCartButton.js
+++ order-food-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CardIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCardButton = props => {
-
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
 
-  return <button className={styles.button} onClick={props.onClick}>
+  useEffect(()=> {
+    if(items.length ===0) {
+      return;
+    }
+
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(()=> {
+      setBtnIsHighLighted(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  }, [items]);
+
+  return <button className={btnClasses} onClick={props.onClick}>
     <span className={styles.icon}>
       <CardIcon />
     </span>
