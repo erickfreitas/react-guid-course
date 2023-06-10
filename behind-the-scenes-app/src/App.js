bd47a4042371ecfa35550a99f4f5d8e10b4faaa8
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-10 10:50:53 -0300

    utilizando React.memo()

--- behind-the-scenes-app/src/App.js
+++ behind-the-scenes-app/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevState) => {
+      return !prevState;
+    });
+  };
+
+  console.log('APP RUNNING!');
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput showParagraph={false}/>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
---
+++ behind-the-scenes-app/src/components/Demo/MyParagraph.js
@@ -0,0 +1,6 @@
+const MyParagraph = props => {
+  console.log('MyParagraph RUNNING!');
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
