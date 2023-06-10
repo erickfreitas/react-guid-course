70bf03c3794a3a458da4be5cef728e18dd6aef91
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-10 12:11:44 -0300

    utilizando useCallBack para controle correto do React.memo()

--- behind-the-scenes-app/src/App.js
+++ behind-the-scenes-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
 
@@ -6,11 +6,19 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevState) => {
-      return !prevState;
-    });
+  const toggleParagraphHandler = useCallback(() => {
+    if(allowToggle) {
+      setShowParagraph((prevState) => {
+        return !prevState;
+      });
+    }
+    
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
   console.log('APP RUNNING!');
@@ -18,7 +26,9 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput showParagraph={false}/>
+      <DemoOutput showParagraph={showParagraph}/>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <hr />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
