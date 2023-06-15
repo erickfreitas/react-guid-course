222e0a12b619d1a793d857af5776baa4cc3171c6
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-15 16:29:15 -0300

    configurando custom hook

--- counter-app/src/components/ForwardCounter.js
+++ counter-app/src/components/ForwardCounter.js
@@ -2,7 +2,7 @@ import Card from './Card';
 import useCounter from '../hooks/use-counter';
 
 const ForwardCounter = () => {
-  const counter = useCounter();
+  const counter = useCounter(true);
   return <Card>{counter}</Card>;
 };
 
--- counter-app/src/hooks/use-counter.js
+++ counter-app/src/hooks/use-counter.js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forward = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forward) {
+        setCounter((prevCounter) => prevCounter + 1);
+      }
+      else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forward]);
 
   return counter;
 }
