5625bdb34dd327b9fdd750298a53f598bd436766
erickfreitas <erickfreitasbezerra@gmail.com>
2023-06-26 21:37:46 -0300

    exibindo loading e tratamento de erro no request http

--- complete-order-food-app/src/components/Meals/AvailableMeals.js
+++ complete-order-food-app/src/components/Meals/AvailableMeals.js
@@ -6,10 +6,17 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://movies-react-app-ebf33-default-rtdb.firebaseio.com/meals.json').then();
+
+      if(!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -24,10 +31,27 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
     
-    fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return <section className={styles.mealsLoading}>
+      <p>Loading...</p>
+    </section>;
+  }
+
+  if (httpError) {
+    return <section className={styles.mealsError}>
+      <p>{httpError}</p>
+    </section>;
+  }
 
   const mealsList = meals.map(meal => {
     return <MealItem
