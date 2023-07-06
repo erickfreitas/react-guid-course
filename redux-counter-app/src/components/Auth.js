a6c356ca0d0658f42f42c6165529c7ce00901e9a
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-06 20:56:13 -0300

    separando cada Redux Slice em arquivos diferentes

--- redux-counter-app/src/components/Auth.js
+++ redux-counter-app/src/components/Auth.js
@@ -1,39 +1,30 @@
 import classes from "./Auth.module.css";
-import { useSelector, useDispatch } from "react-redux";
-import { authActions } from "../store/index";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
 
 const Auth = () => {
   const dispatch = useDispatch();
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   const loginHandler = (event) => {
     event.preventDefault();
     dispatch(authActions.login());
   };
 
-  let authContent = (
-    <form onSubmit={loginHandler}>
-      <div className={classes.control}>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-      </div>
-      <button>Login</button>
-    </form>
-  );
-
-  if (isAuthenticated) {
-    authContent = (
-      <h2>Welcome! You are logged in</h2>
-    );
-  }
-
   return (
     <main className={classes.auth}>
-      <section>{authContent}</section>
+      <section>
+        <form onSubmit={loginHandler}>
+          <div className={classes.control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <button>Login</button>
+        </form>
+      </section>
     </main>
   );
 };
