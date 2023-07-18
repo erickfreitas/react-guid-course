8ecdb1da902b14389e8f1bc7192fb6c2d02a3089
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-18 16:54:53 -0300

    utilizando useEffect com Redux

--- redux-advanced-app/src/App.js
+++ redux-advanced-app/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,17 @@ import Products from './components/Shop/Products';
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://movies-react-app-ebf33-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
