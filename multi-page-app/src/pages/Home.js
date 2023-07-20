c0a4deec48b73f34e69d8fdb77be9f101f26a561
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-20 14:43:39 -0300

    navegando de forma programática

--- multi-page-app/src/pages/Home.js
+++ multi-page-app/src/pages/Home.js
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
-      <p>Go to <Link to="/products">the list of products</Link></p>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Go to the list of products</button>
+      </p>
     </>
   );
 };
