f8af1ac7e412aed6bddaea280cd9b8e595abbd0b
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-20 07:59:03 -0300

    criando menu de navegação e uma página de layout

--- multi-page-app/src/App.js
+++ multi-page-app/src/App.js
@@ -1,26 +1,33 @@
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
-const routerDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routerDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage/>
-//   },
-//   {
-//     path: "/products",
-//     element: <ProductsPage/>
-//   }
-// ]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage/>
+      },
+      {
+        path: "/products",
+        element: <ProductsPage/>
+      }
+    ]
+  },
+]);
 
-const router = createBrowserRouter(routerDefinition);
+//const router = createBrowserRouter(routerDefinition);
 
 function App() {
   return <RouterProvider router={router} />;
