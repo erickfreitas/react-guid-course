36c92c466c2c36e8799e8d25a9861bedbd87761a
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-20 07:34:42 -0300

    outra maneira de definir rotas

--- multi-page-app/src/App.js
+++ multi-page-app/src/App.js
@@ -1,17 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>
-  },
-  {
-    path: "/products",
-    element: <ProductsPage/>
-  }
-]);
+const routerDefinition = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage/>
+//   },
+//   {
+//     path: "/products",
+//     element: <ProductsPage/>
+//   }
+// ]);
+
+const router = createBrowserRouter(routerDefinition);
 
 function App() {
   return <RouterProvider router={router} />;
