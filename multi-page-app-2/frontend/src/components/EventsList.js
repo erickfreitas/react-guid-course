5fe87b32e1c736b03b73efa064c7d8db918d8987
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-21 19:41:46 -0300

    utilizando dados do loader em um component filho

--- multi-page-app-2/frontend/src/components/EventsList.js
+++ multi-page-app-2/frontend/src/components/EventsList.js
@@ -1,6 +1,10 @@
-import classes from './EventsList.module.css';
+import { useLoaderData } from "react-router-dom";
 
-function EventsList({ events }) {
+import classes from "./EventsList.module.css";
+
+function EventsList() {
+  const events = useLoaderData();
+
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
