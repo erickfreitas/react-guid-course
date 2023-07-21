eaf26f4c674d9637af387436f267bbc6791dbce8
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-21 20:25:27 -0300

    retornando response na função loader

--- multi-page-app-2/frontend/src/pages/RootLayout.js
+++ multi-page-app-2/frontend/src/pages/RootLayout.js
@@ -2,13 +2,13 @@ import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
 
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading...</p>}
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
