52cb935c77e4dd08e9a742a093b00b8500ebac60
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-31 19:21:29 -0300

    trabalhando com Query Parameters

--- authentication-app/frontend/src/components/AuthForm.js
+++ authentication-app/frontend/src/components/AuthForm.js
@@ -1,31 +1,27 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import { Form, Link, useSearchParams } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
 
   return (
     <>
-      <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+      <Form method='post' className={classes.form}>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='email' name='email' required />
         </p>
         <p>
-          <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
+          <label htmlFor='image'>Password</label>
+          <input id='password' type='password' name='password' required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          <Link to={`?mode=${!isLogin ? "login" : "signup"}`} type='button'>
+            {isLogin ? "Create new user" : "Login"}
+          </Link>
           <button>Save</button>
         </div>
       </Form>
