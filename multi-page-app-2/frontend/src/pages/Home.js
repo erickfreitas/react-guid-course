3ad41691f187645a4aa5947796eeb6d5e8ae79c2
erickfreitas <erickfreitasbezerra@gmail.com>
2023-07-31 18:33:35 -0300

    incluíndo projeto authentication-app

--- multi-page-app-2/frontend/src/pages/Home.js
+++ multi-page-app-2/frontend/src/pages/Home.js
@@ -1,5 +1,11 @@
+import PageContent from "../components/PageContent";
+
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <PageContent title='Home Page'>
+      <p>Browser all our amazing events!</p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
