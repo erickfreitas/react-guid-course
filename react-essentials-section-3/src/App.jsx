2640ffb7148d1b7037c99ac87c1460be13a0966d
erickfreitas <erickfreitasbezerra@gmail.com>
2023-10-25 20:35:27 -0300

    passando argumentos para Event Functions

--- react-essentials-section-3/src/App.jsx
+++ react-essentials-section-3/src/App.jsx
@@ -5,8 +5,8 @@ import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
-  function handleClick() {
-    console.log('tab button clicked');
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
   }
 
   return (
@@ -33,10 +33,34 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton
+              onClick={() => {
+                handleClick('components');
+              }}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onClick={() => {
+                handleClick('jsx');
+              }}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              onClick={() => {
+                handleClick('props');
+              }}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onClick={() => {
+                handleClick('state');
+              }}
+            >
+              State
+            </TabButton>
           </menu>
         </section>
       </main>
