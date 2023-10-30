fa6515360a38f1f6a07ffa9f65ae940ac7274ab3
erickfreitas <erickfreitasbezerra@gmail.com>
2023-10-30 20:25:54 -0300

    utilizando useState Hook

--- react-essentials-section-3/src/App.jsx
+++ react-essentials-section-3/src/App.jsx
@@ -1,12 +1,31 @@
-import { CORE_CONCEPTS } from './data';
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import TabButton from './components/TabButton';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
   function handleClick(selectedButton) {
     console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -62,6 +81,8 @@ function App() {
               State
             </TabButton>
           </menu>
+
+          {tabContent}
         </section>
       </main>
     </div>
