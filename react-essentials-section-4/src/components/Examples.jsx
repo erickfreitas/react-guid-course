2912602cb9d3612a06151d1c9de048c2be9a3517
erickfreitas <erickfreitasbezerra@gmail.com>
2023-11-21 19:47:00 -0300

    passando um tipo de componente dinamicamente

--- react-essentials-section-4/src/components/Examples.jsx
+++ react-essentials-section-4/src/components/Examples.jsx
@@ -31,34 +31,33 @@ export default function Examples() {
   return (
     <Section title='Examples' id='examples'>
       <Tabs
+        buttonsContainer='menu'
         buttons={
           <>
-            <menu>
-              <TabButton
-                isSelected={selectedTopic === 'components'}
-                onClick={() => handleSelect('components')}
-              >
-                Components
-              </TabButton>
-              <TabButton
-                isSelected={selectedTopic === 'jsx'}
-                onClick={() => handleSelect('jsx')}
-              >
-                JSX
-              </TabButton>
-              <TabButton
-                isSelected={selectedTopic === 'props'}
-                onClick={() => handleSelect('props')}
-              >
-                Props
-              </TabButton>
-              <TabButton
-                isSelected={selectedTopic === 'state'}
-                onClick={() => handleSelect('state')}
-              >
-                State
-              </TabButton>
-            </menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onClick={() => handleSelect('components')}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onClick={() => handleSelect('jsx')}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onClick={() => handleSelect('props')}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onClick={() => handleSelect('state')}
+            >
+              State
+            </TabButton>
           </>
         }
       >
