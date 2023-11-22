6a25c97abad664ea7c5b35591f916ec4511bdb83
erickfreitas <erickfreitasbezerra@gmail.com>
2023-11-22 20:03:55 -0300

    implementando edição de campo two-way binding

--- tic-tac-toe-app/src/App.jsx
+++ tic-tac-toe-app/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <main>
       <div id='game-container'>
         <ol id='players'>
-          <Player name='Player 1' symbol='X'></Player>
-          <Player name='Player 2' symbol='O'></Player>
+          <Player initialName='Player 1' symbol='X'></Player>
+          <Player initialName='Player 2' symbol='O'></Player>
         </ol>
         GAME BOARD
       </div>
--- tic-tac-toe-app/src/components/Player.jsx
+++ tic-tac-toe-app/src/components/Player.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditButton() {
-    setIsEditing(!isEditing);
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
   }
 
   return (
     <li>
       <span className='player'>
-        {!isEditing && <span className='player-name'>{name}</span>}
-        {isEditing && <input type='text' required value={name} />}
+        {!isEditing && <span className='player-name'>{playerName}</span>}
+        {isEditing && (
+          <input
+            type='text'
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
         <span className='player-symbol'>{symbol}</span>
       </span>
       <button onClick={handleEditButton}>{isEditing ? 'Save' : 'Edit'}</button>
