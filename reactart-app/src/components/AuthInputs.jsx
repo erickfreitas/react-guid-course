ca5dffd058eb34c5b341796119a884921a580633
erickfreitas <erickfreitasbezerra@gmail.com>
2024-01-02 15:36:35 -0300

    estilizando aplicação com vanilla CSS, inline css e classes css

--- reactart-app/src/components/AuthInputs.jsx
+++ reactart-app/src/components/AuthInputs.jsx
@@ -21,20 +21,23 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id='auth-inputs'>
+      <div className='controls'>
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : undefined}`}>
+            Email
+          </label>
           <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            type='email'
+            // className={emailNotValid ? 'invalid' : undefined}
+            style={{ background: emailNotValid ? 'red' : 'white' }}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
           <label>Password</label>
           <input
-            type="password"
+            type='password'
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
@@ -42,11 +45,13 @@ export default function AuthInputs() {
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className='button' onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
