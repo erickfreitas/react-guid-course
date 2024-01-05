c09b0447f62d8e8b164a13f1f93e105e0bbc3abe
erickfreitas <erickfreitasbezerra@gmail.com>
2024-01-04 22:07:24 -0300

    instalando e utilizando tailwind no projeto

--- reactart-app/src/components/Input.jsx
+++ reactart-app/src/components/Input.jsx
@@ -1,32 +1,21 @@
 import { styled } from 'styled-components';
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => (props.$invalid ? '#f87171' : '#6b7280')};
-`;
+export default function Input({ label, invalid, ...props }) {
+  let labelClasses = 'block mb-2 text-xs font-bold tracking-wide uppercase';
+  let inputClasses = 'w-full px-3 py-2 leading-tight  border rounded shadow';
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${(props) => (props.$invalid ? '#fed2d2' : '#d1d5db')};
-  color: ${(props) => (props.$invalid ? '#ef4444' : '#374151')};
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  border-color: ${(props) => (props.$invalid ? '#f73f3f' : 'transparent')};
-`;
+  if (invalid) {
+    labelClasses += ' text-red-400';
+    inputClasses += 'text-red-700 bg-red-100 border-red-300';
+  } else {
+    labelClasses += ' text-stone-400';
+    inputClasses += 'text-gray-700 bg-stone-300';
+  }
 
-export default function CustomInput({ label, invalid, ...props }) {
   return (
     <p>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...props} />
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
     </p>
   );
 }
