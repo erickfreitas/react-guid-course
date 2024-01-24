62c331994bb50bd1e0ec65128f2f7a2fdead6aad
erickfreitas <erickfreitasbezerra@gmail.com>
2024-01-24 16:47:21 -0300

    capturando nome do jogador com state two-way-binding

--- countdown-app/src/components/Player.jsx
+++ countdown-app/src/components/Player.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSetName = (event) => {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {submitted ? playerName : 'Unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type='text' onChange={handleSetName} />
+        <button onClick={handleSubmit} disabled={!playerName}>
+          Set Name
+        </button>
       </p>
     </section>
   );
