f12208474769de821a276dae54aa8bbafbaa70c4
erickfreitas <erickfreitasbezerra@gmail.com>
2024-02-14 12:11:12 -0300

    criando components de cadastro de projetos e setando classes do tailwind

--- management-app/src/App.jsx
+++ management-app/src/App.jsx
@@ -1,8 +1,12 @@
+import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className='h-screen my-8 flex gap-8'>
+      <ProjectsSidebar />
+      <NewProject />
+    </main>
   );
 }
 
